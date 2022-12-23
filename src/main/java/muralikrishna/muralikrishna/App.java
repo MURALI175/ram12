e77cf28383c0dbe1078ee89a6f4a2e9db0f0773d
MURALI175 <muralikrishnapenta@1998.gmail.com>
2022-12-23 16:42:46 +0530

    first commit

---
+++ src/main/java/muralikrishna/muralikrishna/App.java
@@ -0,0 +1,13 @@
+package muralikrishna.muralikrishna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/murali/krishna.java
@@ -0,0 +1,6 @@
+package murali;
+
+public class krishna {
+	//hii sir my name is muralikrishna s/o sriramulu
+
+}
